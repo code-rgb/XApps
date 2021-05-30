db5fdc5d7de6e9544387db6ffa28a623f2f80118
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-05-30 15:40:26 +0530

    Update main.py

--- xapps/main.py
+++ xapps/main.py
@@ -42,7 +42,3 @@ async def main():
         await session.close()
         await apk_dl.stop()
 
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
