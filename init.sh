d82c2e9edac3cc05ca1de63161fb875b5dc4398c
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-08-27 00:13:19 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -30,5 +30,5 @@ echo  "     OS      :  Linux"
 mkdir -p build && cd build
 curl -s $(get_source $latest) | tar -xz
 source_dir="MediaInfo_CLI_GNU_FromSource"
-[ -d $source_dir ] && cd source_dir || exit 1
+[ -d $source_dir ] && cd $source_dir || exit 1
 chmod +x CLI_Compile.sh && ./CLI_Compile.sh
